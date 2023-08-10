c41d4367b6bd168edcf28d6786fb67dcb672c777
Prinzuswahn <140445171+Prinzuswahn@users.noreply.github.com>
2023-08-10 19:18:42 +0200

    test upload

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // test
+        // 1
         System.out.println("Mein Taschenrechner: ");
 
         // Einlesen von zwei Zahlen
