48cfe93cf45d6c9bfd89362d2b643c6cf00f8e62
Prinzuswahn <140445171+Prinzuswahn@users.noreply.github.com>
2023-08-12 13:00:09 +0200

    Weiterentwicklung Calculator

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // T23
+        // Begrüßung
         System.out.println("Mein Taschenrechner: ");
 
         // Einlesen von zwei Zahlen
