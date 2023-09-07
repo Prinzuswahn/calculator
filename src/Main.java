6edd9cff8b72dcc547ebd2017cc1291834ee1fd8
Prinzuswahn <140445171+Prinzuswahn@users.noreply.github.com>
2023-09-07 15:01:05 +0200

    Endlose Berechnung aufeinanderfolgenden Zahlen, noch ohne korrekte Berechnung

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,45 @@ public class Main {
     public static void main(String[] args) {
 
         // Begrüßung1
-        System.out.println("Mein Taschenrechner: ");
+        System.out.println("Willkommen bei meinem Taschenrechner: \n");
 
         // Einlesen von zwei Zahlen
 
         System.out.println("Die erste Zahl bitte:\t ");
         Scanner ersteZahl = new Scanner(System.in);
-        int zahl1 = ersteZahl.nextInt();
-
-        System.out.println("Die zweite Zahl bitte:\t ");
-        Scanner zweiteZahl = new Scanner(System.in);
-        int zahl2 = zweiteZahl.nextInt();
+        double zahl1 = ersteZahl.nextInt();
 
         System.out.println("Welche Operation soll durchgeführt werden? +, -, *, /\t ");
         Scanner dritte = new Scanner(System.in);
-        String operator  = dritte.nextLine();
+        String operator = dritte.nextLine();
 
-        // Ergebnis der zwei Zahlen in var ergebnis
+        System.out.println("Die zweite Zahl bitte:\t ");
+        Scanner zweiteZahl = new Scanner(System.in);
+        double zahl2 = zweiteZahl.nextInt();
 
-        /* Ausgabe von 1. und 2. Zahl + anschließender Ausgabe der Summe
+        // Variablen für Zwischenergebnisse
+        double addition = zahl1 + zahl2;
+        double subtraktion = zahl1 - zahl2;
+        double multiplikation = zahl1 * zahl2;
+        double division = zahl1 / zahl2;
 
-        System.out.println("Erste Zahl ist: " + zahl1 + " \nZweite Zahl ist: " + zahl2);
-        System.out.println("\nDas Ergebnis von " + zahl1 + " + " + zahl2 + " ist " + ergebnis ); */
 
         switch (operator) {
             case "+":
                 System.out.println("Das Ergebnis: ");
-                System.out.println(zahl1 + zahl2);
+                System.out.println(addition);
                 break;
             case "-":
                 System.out.println("Das Ergebnis: ");
-                System.out.println(zahl1 - zahl2);
+                System.out.println(subtraktion);
                 break;
             case "*":
                 System.out.println("Das Ergebnis: ");
-                System.out.println(zahl1 * zahl2);
+                System.out.println(multiplikation);
                 break;
             case "/":
                 System.out.println("Das Ergebnis: ");
-                System.out.println(zahl1 / zahl2);
+                System.out.println(division);
                 break;
             default:
                 System.out.println("Ungueltige Angabe!");
@@ -50,53 +50,52 @@ public class Main {
 
         }
 
+
         System.out.println("Möchtest Du weiter rechnen? y/n");
-            Scanner decision = new Scanner(System.in);
-            String input = decision.next();
+        Scanner decision = new Scanner(System.in);
+        String input = decision.next();
 
-        if ( "y".equals (input)) {
-            System.out.println("Gib eine weitere Operation ein: +, -, *, /\t ");
+    while (true) {
 
+        if ("y".equals(input)) {
+
+            System.out.println("Gib eine Operation ein: +, -, *, /\t ");
             Scanner vierte = new Scanner(System.in);
-            String operator1  = vierte.nextLine();
+            String operator2 = vierte.nextLine();
 
-            System.out.println("Die dritte Zahl bitte:\t ");
+            System.out.println("Die nächste Zahl bitte:\t ");
             Scanner dritteZahl = new Scanner(System.in);
-            int zahl3 = dritteZahl.nextInt();
+            double zahl3 = dritteZahl.nextInt();
 
-            System.out.println("Die vierte Zahl bitte:\t ");
-            Scanner vierteZahl = new Scanner(System.in);
-            int zahl4 = vierteZahl.nextInt();
 
-            switch (operator1) {
-                case "+":
-                    System.out.println("Das Ergebnis: ");
-                    System.out.println(zahl1 + zahl2 + zahl3 + zahl4);
-                    break;
-                case "-":
-                    System.out.println("Das Ergebnis: ");
-                    System.out.println(zahl1 - zahl2 - zahl3 - zahl4);
-                    break;
-                case "*":
-                    System.out.println("Das Ergebnis: ");
-                    System.out.println(zahl1 * zahl2 * zahl3 * zahl4);
-                    break;
-                case "/":
-                    System.out.println("Das Ergebnis: ");
-                    System.out.println(zahl1 / zahl2 / zahl3 / zahl4);
-                    break;
-                default:
-                    System.out.println("Ungueltige Angabe!");
-                    break;
+        switch (operator2) {
+            case "+":
+                System.out.println("Das Ergebnis: ");
+                System.out.println(addition + zahl3);
+                break;
+            case "-":
+                System.out.println("Das Ergebnis: ");
+                System.out.println(subtraktion - zahl3);
+                break;
+            case "*":
+                System.out.println("Das Ergebnis: ");
+                System.out.println(multiplikation * zahl3);
+                break;
+            case "/":
+                System.out.println("Das Ergebnis: ");
+                System.out.println(division / zahl3);
+                break;
+            default:
+                System.out.println("Ungueltige Angabe!");
+                break;
 
-            }
-
-        } else {
-            System.out.println("Eingabe beendet");
         }
 
+    } else {
 
+        System.out.println("Ungueltige Angabe!");
 
+         }
+            }
+        }
     }
-
-}
