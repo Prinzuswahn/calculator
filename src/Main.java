36ff217092bf3d4515a42d6a85d736cc0401139e
Prinzuswahn <140445171+Prinzuswahn@users.noreply.github.com>
2023-09-07 15:05:41 +0200

    Endlose Berechnung aufeinanderfolgenden Zahlen, noch ohne korrekte Berechnung

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Begrüßung
+        // Begrüßung1
         System.out.println("Willkommen bei meinem Taschenrechner: \n");
 
         // Einlesen von zwei Zahlen
